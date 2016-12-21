b2b14e68ff353adb57109aac1aad922ee3c18b62
Xinqian liu <crystalynliu@163.com>
2016-12-21 19:07:42 +0800

    adjust order about js function

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
   var language = (navigator.userLanguage || navigator.language).toLowerCase();
   
-  if(language === 'zh-cn') {
-    showContent('zh');
-    hideContent('en');
-    if(is_weixin()) {
-      showTip('zh');
-    }
-  } else {
-    showContent('en');
-    hideContent('zh');
-    if(is_weixin()) {
-      showTip('en');
-    }
-  }
-
   function is_weixin() {
       var ua = navigator.userAgent.toLowerCase();
       return ua.match(/MicroMessenger/i) === "micromessenger"; 
   }
-  
+
   function showContent (language){
     var content = document.getElementById(language + '-content');
     content.style.display = 'block';
@@ -38,3 +24,18 @@
     var weixinTip = document.getElementById(language + '-tip');
     weixinTip.style.display = 'none';
   }
+
+  if(language === 'zh-cn') {
+    showContent('zh');
+    hideContent('en');
+    if(is_weixin()) {
+      showTip('zh');
+    }
+  } else {
+    showContent('en');
+    hideContent('zh');
+    if(is_weixin()) {
+      showTip('en');
+    }
+  }
+  
